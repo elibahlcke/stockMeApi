4c319c41752faab56863187271ca5063020a5181
eliBahlcke <elianabahlcke@gmail.com>
2022-12-04 10:24:00 -0300

    First commit

---
+++ api/models/StockModel.js
@@ -0,0 +1,15 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var StockSchema = new Schema({
+    _id: { type: Number },
+    codigo: { type: String},
+    color: { type: String},
+    talle: { type: String},
+    precio: { type: Number},
+    cantidad: { type: Number},
+    fecha: {type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('Stock', StockSchema);
