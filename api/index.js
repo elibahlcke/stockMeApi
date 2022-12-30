07fdf403570f623266f7f82aa64fd22a685bedd9
eliBahlcke <elianabahlcke@gmail.com>
2022-12-30 12:18:46 -0300

    change index location

---
+++ api/index.js
@@ -0,0 +1,37 @@
+var express = require("express"),
+	cors = require("cors"),
+	app = express(),
+	port = process.env.PORT || 8000,
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser");
+var config = require("../config.js");
+
+app.use(cors());
+try {
+	mongoose.connect(
+	process.env.DB_URI,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+);
+} catch (error) {
+	console.log(error)
+}
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+   console.log("Connected succesfuly");
+});
+
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
+app.use(bodyParser.json());
+var routes = require("./routes/StockRoute.js");
+routes(app);
+
+app.listen(port);
+console.log("server running on" + port);
